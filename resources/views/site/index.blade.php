7616237b3d1679c8a08b1fe323b8bab49364dd70
Zikruljack <matakaca.mz30@gmail.com>
2025-02-08 23:22:52 +0700

    style: :lipstick: add bootstrap and jquery

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/libs/bootstrap/css/bootstrap.min.css">
+    <title>SMAN 3 Banda Aceh | Sekolah</title>
+</head>
+
+<body class="layout-fixed  bg-body-tertiary">
+    <div class="app">
+        @include('site.header')
+    </div>
+
+    <script src="/assets/libs/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
